1403af33382e65a61fd0eb2ada1ac51727e5d7be
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-06-23 16:48:06 +0600

    readme link updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Live__Website
+# Live__Website [https://facebook-assessment.web.app/]
+# Client__Code [https://github.com/MahfujAhsan/facebook-assessment-client]
+# Server__Code [https://github.com/MahfujAhsan/facebook-assessment-server]
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,27 +11,52 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <nav className='header'>
-            <div className='header__icons header__icon--active'>
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                padding: "0 30px",
+                cursor: "pointer"
+            }} className='header__icons header__icon--active'>
                 <Link className='icon'  to="/">
                     <TiHome fontSize='35px' />
                 </Link>
             </div>
-            <div className="header__icons">
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                padding: "0 30px",
+                cursor: "pointer"
+            }} className="header__icons">
                 <Link className='icon' to="/pages">
                     <CgFlagAlt fontSize='35px' />
                 </Link>
             </div>
-            <div className="header__icons">
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                padding: "0 30px",
+                cursor: "pointer"
+            }} className="header__icons">
                 <Link className='icon' to="/watch">
                     <MdOutlineSubscriptions fontSize='35px' />
                 </Link>
             </div>
-            <div className="header__icons">
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                padding: "0 30px",
+                cursor: "pointer"
+            }} className="header__icons">
                 <Link className='icon' to="/groups">
                     <MdOutlineSupervisedUserCircle fontSize='35px' />
                 </Link>
             </div>
-            <div className="header__icons">
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                padding: "0 30px",
+                cursor: "pointer"
+            }} className="header__icons">
                 <Link className='icon' to="/gaming">
                     <SiFacebookgaming fontSize='35px' />
                 </Link>
